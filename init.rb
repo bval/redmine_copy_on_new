63890ff83d5189a0041baba371677bdcf8710582
Brandon Valentine <brandon@brandonvalentine.com>
2010-10-23 13:51:42 -0500

    Empty plugin skeleton

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_copy_on_new do
+  name 'Redmine Copy On New plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
