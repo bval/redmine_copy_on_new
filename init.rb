25f1c998b3cd47bc4cee47307a71f20dd8cf0062
Brandon Valentine <brandon@brandonvalentine.com>
2010-10-23 15:35:43 -0500

    Modify plugin name and registration in init.rb, add settings configuration

--- init.rb
+++ init.rb
@@ -1,10 +1,14 @@
 require 'redmine'
 
-Redmine::Plugin.register :redmine_copy_on_new do
-  name 'Redmine Copy On New plugin'
-  author 'Brandon Valentine <brandon@brandonvalentine.com>'
+Redmine::Plugin.register :copy_on_new do
+  name 'Copy On New plugin'
+  author 'Brandon Valentine'
   description 'Allow the administrator to specify a template project from which items are copied when a new project is initialized'
   version '0.0.1'
   url 'http://github.com/brandonvalentine/redmine_copy_on_new'
   author_url 'http://github.com/brandonvalentine'
+
+  settings :default => {
+    'template_project' => 0
+  }, :partial => 'settings/settings'
 end
